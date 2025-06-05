8b5d99a18c5ae0e36fdb4226498243a67579058c
Ian Heath <ianrheath1@gmail.com>
2025-06-05 09:51:57 -0500

    Update azure.py

--- azure.py
+++ azure.py
@@ -1,1 +1,15 @@
-AttributeError: 'JiraAPIWrapper' object has no attribute 'search_issues'. Did you mean: 'parse_issues'?
+from langchain_community.utilities.jira import JiraAPIWrapper
+
+jira = JiraAPIWrapper(
+    jira_instance_url="https://ontrack-internal.amd.com",
+    jira_api_token="MjQ0MzM3NDQzNTY0OsLr4yZLHgekftk2OkuNGC+Ngumk",
+)
+
+issues = jira.run(
+    "project = STXH ORDER BY updated DESC",
+    max_results=2
+)
+
+print("Got", len(issues), "issues")
+for i in issues:
+    print(i.key, i.fields.summary, f"[{i.fields.status.name}]")
