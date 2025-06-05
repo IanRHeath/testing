08051a3f6f18e3c50034a8a73e5c4ada090b2af8
Ian Heath <ianrheath1@gmail.com>
2025-06-05 09:58:14 -0500

    Update azure.py

--- azure.py
+++ azure.py
@@ -1,2 +1,8 @@
- raise AttributeError(f'{type(self).__name__!r} object has no attribute {item!r}')
-AttributeError: 'JiraAPIWrapper' object has no attribute 'client'
+from langchain_community.utilities.jira import JiraAPIWrapper
+
+jira = JiraAPIWrapper(
+    jira_instance_url="https://ontrack-internal.amd.com",
+    jira_api_token   ="MjQ0MzM3NDQzNTY0OsLr4yZLHgekftk2OkuNGC+Ngumk",
+)
+
+print([attr for attr in dir(jira) if attr.startswith("_") or attr.endswith("client")])
