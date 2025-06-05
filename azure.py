854f9756c82bf16bc4438d5292bc86e7963cd6fd
Ian Heath <ianrheath1@gmail.com>
2025-06-05 09:51:16 -0500

    Update azure.py

--- azure.py
+++ azure.py
@@ -1,12 +1,1 @@
-from langchain_community.utilities.jira import JiraAPIWrapper
-
-jira = JiraAPIWrapper(
-    jira_instance_url = "https://ontrack-internal.amd.com",
-    jira_api_token    = "MjQ0MzM3NDQzNTY0OsLr4yZLHgekftk2OkuNGC+Ngumk",
-    # no username for DC PAT
-)
-
-issues = jira.search_issues(jql="project = STXH ORDER BY updated DESC", max_results=2)
-print("Got", len(issues), "issues")
-for i in issues:
-    print(i.key, i.fields.summary, f"[{i.fields.status.name}]")
+AttributeError: 'JiraAPIWrapper' object has no attribute 'search_issues'. Did you mean: 'parse_issues'?
