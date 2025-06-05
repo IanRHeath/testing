4f868cfd6434ab8664a10bf20c4049cd9b54df7b
Ian Heath <ianrheath1@gmail.com>
2025-06-05 09:56:01 -0500

    Update azure.py

--- azure.py
+++ azure.py
@@ -1,15 +1,6 @@
-from langchain_community.utilities.jira import JiraAPIWrapper
-
-jira = JiraAPIWrapper(
-    jira_instance_url="https://ontrack-internal.amd.com",
-    jira_api_token="MjQ0MzM3NDQzNTY0OsLr4yZLHgekftk2OkuNGC+Ngumk",
+issues = jira.client.search_issues(         # note “client”, not “run”
+            jql_str   = "project = STXH ORDER BY updated DESC",
+            maxResults=2                    # camel‑case R (SDK style)
 )
-
-issues = jira.run(
-    "project = STXH ORDER BY updated DESC",
-    max_results=2
-)
-
-print("Got", len(issues), "issues")
 for i in issues:
-    print(i.key, i.fields.summary, f"[{i.fields.status.name}]")
+    print(i.key, i.fields.summary)
