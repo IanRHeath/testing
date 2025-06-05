ffade7e2b78a7e30353c72a9fec188892b43f1c1
Ian Heath <ianrheath1@gmail.com>
2025-06-05 10:02:19 -0500

    Update azure.py

--- azure.py
+++ azure.py
@@ -1,3 +1,4 @@
-issues = jira.run(
-             ^^^^^^^^^
-TypeError: JiraAPIWrapper.run() got an unexpected keyword argument 'Query'
+1 validation error for JiraAPIWrapper
+mode
+  Extra inputs are not permitted [type=extra_forbidden, input_value='server', input_type=str]
+    For further information visit https://errors.pydantic.dev/2.11/v/extra_forbidden
